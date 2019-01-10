43d7fd6dd7fb527408b0615c702964b599efb2d6
Gmrf18 <gmrf18@gmail.com>
2019-01-10 09:39:13 -0600

    Updates

---
+++ precache-manifest.70326eb57d862af288fa484b48317869.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/first-todo-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ac282b0e14eb428d0828",
+    "url": "/first-todo-react/static/js/runtime~main.ac282b0e.js"
+  },
+  {
+    "revision": "e99e3566df4df8469e7d",
+    "url": "/first-todo-react/static/js/main.e99e3566.chunk.js"
+  },
+  {
+    "revision": "c955084f4e3d49388e5b",
+    "url": "/first-todo-react/static/js/1.c955084f.chunk.js"
+  },
+  {
+    "revision": "e99e3566df4df8469e7d",
+    "url": "/first-todo-react/static/css/main.38cead6a.chunk.css"
+  },
+  {
+    "revision": "00445ddfd67b570050352d027fd3f778",
+    "url": "/first-todo-react/index.html"
+  }
+];
